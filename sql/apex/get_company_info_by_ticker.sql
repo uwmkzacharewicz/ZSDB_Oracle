0b3c50022b47e38ebb2d96e8e15c768facc21424
karol <karolzach87@gmail.com>
2025-04-08 19:45:48 +0200

    obsuga flask

--- sql/apex/get_company_info_by_ticker.sql
+++ sql/apex/get_company_info_by_ticker.sql
@@ -1,14 +1,50 @@
 DECLARE
-  l_base_url VARCHAR2(2000) := 'http://192.168.1.200:5001/get-company-info';
-  l_ticker   VARCHAR2(100) := :P2_TICKER;
-  l_url      VARCHAR2(2000);
-  l_response CLOB;
+  l_base_url   VARCHAR2(2000) := 'http://192.168.1.200:5001/get-company-info';
+  l_ticker_in  VARCHAR2(100)  := :P2_FINDTICKER;
+  l_response   CLOB;
+  l_url        VARCHAR2(2000);
+
+  l_name       VARCHAR2(400);
+  l_sector     VARCHAR2(200);
+  l_country    VARCHAR2(100);
+  l_website    VARCHAR2(200);
 BEGIN
-   l_url := l_base_url || '?ticker=' || UTL_URL.ESCAPE(l_ticker);
+  l_url := l_base_url || '?ticker=' || UTL_URL.ESCAPE(l_ticker_in);
+
   l_response := APEX_WEB_SERVICE.make_rest_request(
     p_url         => l_url,
     p_http_method => 'GET'
   );
 
-  :P2_NEW := l_response;
+  l_name    := JSON_VALUE(l_response, '$.name');
+  l_sector  := JSON_VALUE(l_response, '$.sector');
+  l_country := JSON_VALUE(l_response, '$.country');
+  l_website := JSON_VALUE(l_response, '$.website');
+
+  IF l_name IS NULL THEN
+    :P2_ERROR_MSG := 'Nie znaleziono danych dla "' || l_ticker_in || '"';
+
+    -- Wyczyść inne pola
+    :P2_NAME    := NULL;
+    :P2_TICKER  := NULL;
+    :P2_SECTOR  := NULL;
+    :P2_COUNTRY := NULL;
+    :P2_WEBSITE := NULL;
+  ELSE
+    :P2_ERROR_MSG := NULL;
+
+    :P2_NAME    := l_name;
+    :P2_TICKER  := l_ticker_in;
+    :P2_SECTOR  := l_sector;
+    :P2_COUNTRY := l_country;
+    :P2_WEBSITE := l_website;
+  END IF;
+EXCEPTION
+  WHEN OTHERS THEN
+    :P2_ERROR_MSG := 'Wystąpił błąd: ' || SQLERRM;
+    :P2_NAME := NULL;
+    :P2_TICKER := NULL;
+    :P2_SECTOR := NULL;
+    :P2_COUNTRY := NULL;
+    :P2_WEBSITE := NULL;
 END;
--- sql/apex/ping.sql
+++ sql/apex/ping.sql
@@ -21,18 +21,34 @@ BEGIN
   :P2_NEW := l_response;
 END;
 
+
+
 DECLARE
   l_url      VARCHAR2(2000) := 'http://192.168.1.200:5001/update-exchange-rate';
   l_response CLOB;
-  l_rate     NUMBER;
+  l_name VARCHAR2(100);
+    l_ticker VARCHAR2(100);
+    l_sector VARCHAR2(100);
+    l_country VARCHAR2(100);
+    l_website VARCHAR2(100);
 BEGIN
   l_response := APEX_WEB_SERVICE.make_rest_request(
     p_url         => l_url,
     p_http_method => 'GET'
   );
 
-  l_rate := JSON_VALUE(l_response, '$.rate');
-  :P2_NEW := l_rate;
+    l_name := JSON_VALUE(l_response, '$.longName');
+    l_ticker := JSON_VALUE(l_response, '$.ticker');
+    l_sector := JSON_VALUE(l_response, '$.sector');
+    l_country := JSON_VALUE(l_response, '$.country');
+    l_website := JSON_VALUE(l_response, '$.website');
+
+    :P2_NAME := l_name;
+    :P2_TICKER := l_ticker;
+    :P2_SECTOR := l_sector;
+    :P2_COUNTRY := l_country;
+    :P2_WEBSITE := l_website;
+
 END;
 
 
